f729d6458ab8e0e7d7f046857959d91916bab6d2
ilef <ilef7021@gmail.com>
2025-03-10 10:55:48 +0100

    squash merge

--- projet.py
+++ projet.py
@@ -5,5 +5,9 @@ def somme(T):
         return s
 
 data=[1,3,5]
-som=sum(data)
-print('la somme est :',som)
+if data:
+    print('la somme est :',sum(data))
+    print('le min est:',min(data))
+    print('le max est:',max(data))
+else: 
+    print('dossier vide')
